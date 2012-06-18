5e2981790429d611748fbf626fb7c276d3233b88
Patrick Coffey <pcoffey@fahrenheitmarketing.com>
2012-06-18 14:10:47 -0500

    Initial Commit of files

---
+++ html.tpl.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php print $head; ?>
+  <title><?php print $head_title; ?></title>
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+</head>
+<body class="<?php print $classes; ?>" <?php print $attributes;?>>
+  <?php print $page_top; ?>
+  <?php print $page; ?>
+  <?php print $page_bottom; ?>
+</body>
+</html>
