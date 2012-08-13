21c2e05e4ca54570ae0f3e73e22a3a26b66a7bdf
Patrick Coffey <patrick@patrickcoffeymusic.com>
2012-08-12 21:50:43 -0500

    Added jQuery 1.8.0.

--- template.php
+++ template.php
@@ -16,7 +16,7 @@ function BaseBuildingBlocks_process_page(&$vars) {
  * Adds Jquery 1.7 instead of Drupal Default.
  */
 function BaseBuildingBlocks_js_alter(&$javascript) {
-  $javascript['misc/jquery.js']['data'] = drupal_get_path('theme', 'BaseBuildingBlocks') . '/js/vendor/jquery-1.7.2.min.js';
+  $javascript['misc/jquery.js']['data'] = drupal_get_path('theme', 'BaseBuildingBlocks') . '/js/vendor/jquery-1.8.0.min.js';
 }
 
 
