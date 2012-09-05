d06ae3ce7ec941978d7916a3143af1654618e83b
Patrick Coffey <pcoffey@fahrenheitmarketing.com>
2012-09-04 21:25:43 -0500

    Moving <a> inside of the <h1>

--- templates/includes/page-header.php
+++ templates/includes/page-header.php
@@ -7,11 +7,11 @@
   
   <div class="span11 header-text">
     <?php if ($site_name): ?>
-      <a href="<?php print $front_page; ?>">
-        <h1 class="header-title">
+      <h1 class="header-title">
+        <a href="<?php print $front_page; ?>">
           <?php print $site_name; ?>
-        </h1>
-      </a>
+        </a>
+      </h1>
     <?php endif; ?>
     <?php if ($site_slogan): ?>
       <h2 class="header-slogan">
