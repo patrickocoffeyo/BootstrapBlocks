eec03ad88a47090d03706155f732e3b869abfe83
Patrick Coffey <pcoffey@fahrenheitmarketing.com>
2012-10-17 22:19:28 -0500

    Making navbar responsive

--- templates/includes/page-admin-menu.php
+++ templates/includes/page-admin-menu.php
@@ -6,14 +6,18 @@
   <div class="navbar navbar-fixed-top">
     <div class="navbar-inner">
       <div class="container">
+        <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"><i class="icon-reorder"></i></a>
+        
+        
         <?php if ($site_name): ?>
           <a class="brand" href="/"><i class="icon-home"></i></a>
         <?php endif; ?>
-        
-        <ul class="nav">
-          <li class="divider-vertical"></li>
-          <?php print render($admin_menu_expanded); ?>
-        </ul>
+        <div class="nav-collapse">
+          <ul class="nav">
+            <li class="divider-vertical"></li>
+            <?php print render($admin_menu_expanded); ?>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
