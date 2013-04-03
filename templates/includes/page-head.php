a25427698b922bfb2c1a47696e45812deccf5a40
Patrick Coffey <pcoffey@fahrenheitmarketing.com>
2013-04-03 10:56:29 -0500

    Removing logo by default. Cleaning up stylesheet

--- templates/includes/page-head.php
+++ templates/includes/page-head.php
@@ -1,22 +1,14 @@
-<header class="row" id="header" role="banner">
-  <?php if ($logo): ?>
-    <div class="span1 header-logo">
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"></a>
-    </div>
+<header class="container" id="header" role="banner">
+  <?php if ($site_name): ?>
+    <h1 class="header-title">
+      <a href="<?php print $front_page; ?>">
+        <?php print $site_name; ?>
+      </a>
+    </h1>
   <?php endif; ?>
-  
-  <div class="span11 header-text">
-    <?php if ($site_name): ?>
-      <h1 class="header-title">
-        <a href="<?php print $front_page; ?>">
-          <?php print $site_name; ?>
-        </a>
-      </h1>
-    <?php endif; ?>
-    <?php if ($site_slogan): ?>
-      <h2 class="header-slogan">
-        <?php print $site_slogan; ?>
-      </h2>
-    <?php endif; ?>
-  </div>
+  <?php if ($site_slogan): ?>
+    <h2 class="header-slogan">
+      <?php print $site_slogan; ?>
+    </h2>
+  <?php endif; ?>
 </header>
