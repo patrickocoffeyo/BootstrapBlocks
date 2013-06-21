f06c768af83f066a1af75ceb464be7997d8c9981
Patrick Coffey <patrickcoffey48@gmail.com>
2013-06-21 10:23:36 -0500

    Adding code for touch icon management

--- functions/template-functions.php
+++ functions/template-functions.php
@@ -35,12 +35,20 @@ function BootstrapBlocks_footer_scripts() {
  *   Apple Touch Icons in all the right sizes.
  */
 function BootstrapBlocks_touch_icons() {
+  if (theme_get_setting('touch_icons_on_off') == 0) { return; }
+
+  $windows_title = theme_get_setting('windows_title');
+  if (empty($windows_title)) { $windows_title = variable_get('site_name'); }
+
   $path = '/'.drupal_get_path('theme', 'BootstrapBlocks').'/assets/img/icons/';
   $output = '<link rel="apple-touch-icon-precomposed" href="' . $path . 'touch-icon-iphone.png" />' . "\n";
   $output .= '<link rel="apple-touch-icon-precomposed" href="' . $path . 'touch-icon-iphone.png" />' . "\n";
   $output .= '<link rel="apple-touch-icon-precomposed" sizes="72x72" href="' . $path . 'touch-icon-ipad.png" />' . "\n";
   $output .= '<link rel="apple-touch-icon-precomposed" sizes="114x114" href="' . $path . 'touch-icon-iphone4.png" />' . "\n";
   $output .= '<link rel="apple-touch-icon-precomposed" sizes="144x144" href="' . $path . 'touch-icon-ipad3.png" />' . "\n";
+  $output .= '<meta name="application-name" content="' . $windows_title . '"/>';
+  $output .= '<meta name="msapplication-TileColor" content="' . theme_get_setting('windows_color') . '"/>';
+  $output .= '<meta name="msapplication-TileImage" content="' . $path . 'touch-icon-windows.png"/>';
   return $output;
 }
 
