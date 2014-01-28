4f470961145673ef0ff1bc8d05d64003d5fe8c6f
Patrick Coffey <patrickcoffey48@gmail.com>
2014-01-27 20:44:54 -0600

    Header improvements

--- templates/includes/page-head.php
+++ templates/includes/page-head.php
@@ -1,7 +1,11 @@
 <header class="container" id="header" role="banner">
-  <?php if ($site_name): ?>
-    <a class="brand" href="<?php print $front_page; ?>">
-      <?php print $site_name; ?>
-    </a>
-  <?php endif; ?>
+  <h1>
+    <?php if ($site_name): ?>
+      <a class="brand" href="<?php echo $front_page; ?>"><?php echo $site_name; ?></a>
+    <?php endif; ?>
+
+    <?php if ($site_slogan): ?>
+      <small><?php echo $site_slogan; ?></small>
+    <?php endif; ?>
+  </h1>
 </header>
